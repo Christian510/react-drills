3a961562b1a0865ef7197520d524b60d6845a59f
Christian's Mac <macbook@Macbooks-MacBook-Pro.local>
2018-12-13 14:49:27 -0700

    Completed app-1 with my own code - not looking at solution.

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -3,6 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { value: '' };
+    this.handleChange = this.handleChange.bind(this);
+
+  }
+
+  handleChange(event){
+    this.setState({value: event.target.value});
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,12 +23,22 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form>
+          < input onChange={this.handleChange} label="type something" type="text" />
+        </form>
+        <Output typing={this.state.value}/>
+        {/* <p>{this.state.value}</p> */}
+        {/* this works too, but I wanted to use a function for practice */}
+
       </div>
     );
   }
 }
+
+function Output(props){
+  return <p>{props.typing}</p>
+}
+
+
 
 export default App;
--- app-4/src/App.js
+++ app-4/src/App.js
@@ -1,21 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: '',
+      password: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.userName});
+   }
+
+  handleSubmit(event) {
+    alert("A name was submited: " + this.state.userName);
+    event.preventDefault();
+  }
+
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.userName} onChange={this.handleChange} placeholder="Name" />
+          <input type="password" /*value={this.state.password} onChange={}*/  placeholder="Password" />
+          <input type="submit" value="submit" />
+        </form>
+        
       </div>
+
     );
   }
 }
 
 export default App;
+
+
