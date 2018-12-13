364eb55a399fb7b0489314851b3c6a23b1eb30ca
Christian's Mac <macbook@Macbooks-MacBook-Pro.local>
2018-12-13 16:18:47 -0700

    App 2 complete but looked at solution.

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -2,20 +2,38 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+function Groceries(props){
+  return <h1>{props.item}</h1>
+}
+
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      groceries: [
+        "French Bread",
+        "Cheese",
+        "Red Wine",
+        "Tomato Bisque"
+      ]
+    }
+  }
+
   render() {
+   let groceryItems = this.state.groceries.map( (element, index) => {
+      return <h2>{element}</h2>;
+   })
+   
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        { groceryItems }
       </div>
     );
   }
 }
+
+
 
 export default App;
